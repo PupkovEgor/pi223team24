8e2ccff2960db05a18c181c872c2f0e684bcb0d6
vova8 <vova8@DESKTOP-E1KME6E>
2021-04-02 22:31:56 +0500

    Леонтьев изменил Calc.java

--- src/main/java/laba4/Calc.java
+++ src/main/java/laba4/Calc.java
@@ -22,38 +22,47 @@ public class Calc extends HttpServlet {
 	private static class RequestCalc {
 		private final String first_calc;
 		private final String second_calc;
-		private int result;
+		private final String third_calc;
+		private double result;
 						
-		private RequestCalc (String first, String second) {
+		private RequestCalc (String first, String second, String third) {
 			this.first_calc = first;
 			this.second_calc = second;
+			this.third_calc = third;
 			}
 		
 		public static RequestCalc fromRequestParameters(HttpServletRequest request) {
 			return new RequestCalc(
 			request.getParameter("first"),
-			request.getParameter("second"));
+			request.getParameter("second"),
+			request.getParameter("third"));
 			}
 				
 		public void setAsRequestAttributesAndCalculate(HttpServletRequest request) {
 			request.setAttribute("first_result", first_calc);
 			request.setAttribute("second_result", second_calc);
-			int first_try;
-			int second_try;
-			try { 
-			first_try=Integer.parseInt(first_calc);
-			second_try=Integer.parseInt(second_calc);
+			request.setAttribute("third_result", third_calc);
+			double first_try=0;
+			double second_try=0;
+			double third_try=0;
+			try {
+				first_try=Double.parseDouble(first_calc);
+				second_try=Double.parseDouble(second_calc);
+				third_try=Double.parseDouble(third_calc);
 			}
-			catch (NumberFormatException e) {
-				first_try=0;
-				second_try=0;	
+			catch (Exception e) {
+				first_try = 0;
+				second_try= 0;
+				third_try= 0;
 			}
-			
-			result=first_try+second_try;
+			if (first_try>0 & second_try>0 & third_try>0) {
+			result=(second_try*third_try*third_try*first_try)/(12*Math.tan(Math.toRadians(180)/second_try));
+			if (second_try<3) {
+        		result = 0;
+			}
+		}; 
 			request.setAttribute("result", result);
 		}
 		
 	}
-	
-	
 }
